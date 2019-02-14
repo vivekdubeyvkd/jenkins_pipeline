5714d2600a4270db13ea2753563db81e2d8b170f
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-02-14 13:55:54 +0530

    Create checkAndTriggerDownstreamJob.groovy

---
+++ checkAndTriggerDownstreamJob.groovy
@@ -0,0 +1,21 @@
+def checkAndTriggerDownstreamJob(items, jobName) {
+  for (item in items) {
+    if (item.class.canonicalName != 'com.cloudbees.hudson.plugins.folder.Folder') {
+        if(item.fullName == jobName.substring(jobName.indexOf('/') + 1)){
+            if(item.disabled != true){
+                println(item.name + " is enabled and going to trigger it ")
+                build job: jobName, wait: false
+            } else {
+                println(item.name + " is disabled and cannot be triggered ")
+            }  
+        }  
+      
+    } else {     
+        checkAndTriggerDownstreamJob(((com.cloudbees.hudson.plugins.folder.Folder) item).getItems(), jobName)
+    }
+  }
+} 
+
+node{
+    checkAndTriggerDownstreamJob(Hudson.instance.items, '<job-name>')
+}
