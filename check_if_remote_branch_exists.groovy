2603b642ec9c3a8eeb14340596a027ce0ff5de52
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2021-04-21 16:29:32 +0530

    Update check_if_remote_branch_exists.groovy

--- check_if_remote_branch_exists.groovy
+++ check_if_remote_branch_exists.groovy
@@ -2,24 +2,28 @@
 GITHUB_ORG = "<github org name>"
 GITHUB_REPO = "<github repo under above org>"
 
-def check_remote_branch(branch_name){
-    dir('repodir'){
-         remote_branches = ""
-         withCredentials([usernamePassword(credentialsId: 'github_token', passwordVariable: 'GITHUBPWD', usernameVariable: 'GITHUBUSER')]) {
+def check_if_remote_git_branch(branch_name){
+    withCredentials([usernamePassword(credentialsId: 'github_token', passwordVariable: 'GITHUBPWD', usernameVariable: 'GITHUBUSER')]) {
+        GIT_REPO_URL = "https://${GITHUBUSER}:${GITHUBPWD}@github.com/${GITHUB_ORG}/${GITHUB_REPO}" 
+        sh """
+            git clone ${GIT_REPO_URL} repodir
+        """
+        dir('repodir'){
+            remote_branches = ""
             try{
-                sh """
-                   git clone https://${GITHUBUSER}:${GITHUBPWD}@github.com/${GITHUB_ORG}/${GITHUB_REPO}
-                   git remote set-url origin https://${GITHUBUSER}:${GITHUBPWD}@github.com/${GITHUB_ORG}/${GITHUB_REPO}
+               sh """
+                   git remote set-url origin ${GIT_REPO_URL}
                 """
                 remote_branches = sh(returnStdout: true, script: "git ls-remote --heads origin ${branch_name}")
             } catch(err){
                 error "Remote branch ${branch_name} doesn't exists on GitHub origin"
             }
+            
             echo """
-                  Remote branch : ${remote_branches}
+                    Remote branch : ${remote_branches}
             """
         }
-    }
+     }
 }
 
-check_remote_branch("vivek")
+check_if_remote_git_branch("<remote branch name>")
