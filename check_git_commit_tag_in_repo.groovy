5740370384a9f643a7c38326155d7a866185fc01
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2021-04-21 17:56:40 +0530

    Update check_git_commit_tag_in_repo.groovy

--- check_git_commit_tag_in_repo.groovy
+++ check_git_commit_tag_in_repo.groovy
@@ -1,15 +1,17 @@
 
 GIT_COMMIT_OR_TAG = "<a valid GIT commit or Tag>"
 local_cloned_repo_dir = 'repodir'
+GITHUB_BRANCH = "<Gi
 
 def check_git_commmit() {
     if("${GIT_COMMIT_OR_TAG}") {
         dir(local_cloned_repo_dir) {
-            commit_in_source_branch = sh(returnStdout: true, script: "git branch -r --contains ${GIT_COMMIT_OR_TAG} --points-at origin/${SOURCE_BRANCH}").trim()
-            if(commit_in_source_branch =~ "origin/${SOURCE_BRANCH}"){
-                println('commit_in_source_branch ' + commit_in_source_branch)
+            commit_in_github_branch = sh(returnStdout: true, script: "git branch -r --contains ${GIT_COMMIT_OR_TAG} --points-at origin/${GITHUB_BRANCH}").trim()
+            if(commit_in_github_branch =~ "origin/${GITHUB_BRANCH}"){
+                println('commit_in_source_branch ' + commit_in_github_branch)
+                return true
             }
-            error "Commit ID or Tag ${GIT_COMMIT_OR_TAG} is not in origin/${SOURCE_BRANCH} source branch  " 
+            error "Commit ID or Tag ${GIT_COMMIT_OR_TAG} is not in origin/${GITHUB_BRANCH} source branch  " 
         }
     } 
 }
